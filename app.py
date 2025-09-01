ada7bd992aa83cf1c7af2801a1c30b9a5f4ed4e0
zainabrafique123 <zainabrafique2004@gmail.com>
2025-09-01 22:08:04 +0500

    Initial commit

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def hello():
     return "Hello, Flask is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
